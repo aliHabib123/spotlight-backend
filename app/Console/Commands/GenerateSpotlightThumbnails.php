3363d9c377cd9211fe86e21866bb7464d0af5b22
Ali Habib Own <ali.h@sela.ae>
2025-09-16 18:32:47 +0400

    feat: increase memory limit to 512MB for image processing commands

--- app/Console/Commands/GenerateSpotlightThumbnails.php
+++ app/Console/Commands/GenerateSpotlightThumbnails.php
@@ -31,7 +31,12 @@ class GenerateSpotlightThumbnails extends Command
      */
     public function handle()
     {
+        // Set memory limit for image processing
+        ini_set('memory_limit', '512M');
+        set_time_limit(300);
+        
         $this->info('Starting thumbnail generation for spotlights...');
+        $this->info('Memory limit set to: ' . ini_get('memory_limit'));
 
         // Get spotlights to process
         $query = Spotlight::whereNotNull('featured_image');
